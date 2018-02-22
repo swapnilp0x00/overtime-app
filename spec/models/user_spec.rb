b92f25eaf79a8f108dd5af4ddf70dd651c8a2343
swapnil <ddevil691@gmail.com>
2018-02-22 13:05:03 +0530

    user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(email:"test@test.com",password:"astfgh",password_confirmation:"astfgh",first_name:"first_name",last_name:"last_name")
+  end
+  it "can be created" do
+    expect(@user).to be_valid
+  end
+
+  it "cant be created without firstname" do
+    @user.first_name = nil
+    @user.last_name = nil
+    expect(@user).to_not be_valid
+  end
 end
